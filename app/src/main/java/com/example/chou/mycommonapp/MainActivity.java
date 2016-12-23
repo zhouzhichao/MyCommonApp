c9ea019adf7604b2bab44bfc7fdf88ac483391cb
zhouzc <437160332@qq.com>
2016-12-23 17:35:14 +0800

    git commit

--- app/src/main/java/com/example/chou/mycommonapp/MainActivity.java
+++ app/src/main/java/com/example/chou/mycommonapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.chou.mycommonapp;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.AbsoluteSizeSpan;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private View c2;
     private TextView tv1;
     private TextView tv2;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +34,16 @@ public class MainActivity extends AppCompatActivity {
         c2 = ((View) findViewById(R.id.c2));
         tv1 = ((TextView) c1.findViewById(R.id.tv_list_item));
         tv2 = ((TextView) c2.findViewById(R.id.tv_list_item));
+        tv = ((TextView) findViewById(R.id.tv));
         tv1.setCompoundDrawables(getResources().getDrawable(R.mipmap.icon_wallet),null,getResources().getDrawable(R.mipmap.icon_wallet),null);
         tv2.setCompoundDrawables(getResources().getDrawable(R.mipmap.icon_wallet),null,getResources().getDrawable(R.mipmap.icon_wallet),null);
+        String text = String.format("￥%1$s  门市价:￥%2$s", 18.6, 22);
+        int z = text.lastIndexOf("门");
+        SpannableStringBuilder style = new SpannableStringBuilder(text);
+        style.setSpan(new AbsoluteSizeSpan(14), 0, 1, Spannable.SPAN_EXCLUSIVE_INCLUSIVE); //字号
+        style.setSpan(new ForegroundColorSpan(Color.parseColor("#afafaf")), z, text.length(), Spannable.SPAN_EXCLUSIVE_INCLUSIVE); //颜色
+        style.setSpan(new AbsoluteSizeSpan(22), z, text.length(), Spannable.SPAN_EXCLUSIVE_INCLUSIVE); //字号
+
+        tv.setText(style);
     }
 }
---
+++ app/src/main/java/com/example/chou/mycommonapp/utils/DisplayUtil.java
@@ -0,0 +1,13 @@
+package com.example.chou.mycommonapp.utils;
+
+import android.content.Context;
+
+/**
+ * Created by Chou on 2016/12/23.
+ */
+
+public class DisplayUtil {
+    public static void dip2px(Context context,int dp){
+
+    }
+}
